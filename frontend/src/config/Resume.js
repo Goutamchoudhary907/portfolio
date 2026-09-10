60b492d2f6a76ee61b64d0a8489b2130447ce506
Goutam Choudhary <goutamchoudhary907@gmail.com>
2026-09-10 23:31:19 +0530

    resume update

--- frontend/src/config/Resume.js
+++ frontend/src/config/Resume.js
@@ -1,3 +1,3 @@
 export const resumeConfig = {
-  url: ' https://drive.google.com/file/d/1IGHRnPKYQ8KvhgNHld_PHj_op9BtzxaF/view',
+  url: 'https://drive.google.com/file/d/1IGHRnPKYQ8KvhgNHld_PHj_op9BtzxaF/preview',
 };
